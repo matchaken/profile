fa03c7a0909cac49f7432360d41481c9fed0180b
MatchaLatte <nawamongkol.bank@gmail.com>
2023-06-30 15:45:06 +0700

    ::> [~] setting config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: '/nextjs-website',
+  assetPrefix: '/nextjs-website'
+}
 
 module.exports = nextConfig
