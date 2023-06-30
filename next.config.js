3ca32ef8440d1e1872c2646a3d6ceefde1924731
MatchaLatte <nawamongkol.bank@gmail.com>
2023-06-30 16:09:18 +0700

    [~] remove base path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/nextjs-website',
-  assetPrefix: '/nextjs-website'
+  output: 'export'
 }
 
 module.exports = nextConfig
